1ae29b34a8a1858a69760513336160e0e6961018
Jeremy LaRose <jeremy@jeremylarose.com>
2021-07-06 14:26:44 -0400

    Initial Commit

---
+++ InventoryAPI/Models/Client.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace InventoryAPI.Models
+{
+    public class Client
+    {
+        public Guid Id { get; init; }
+        public string Name { get; init; }
+        public decimal Price { get; init; }
+        public int HorseCount { get; set; }
+        public string Location { get; set; }
+        public DateTimeOffset CreatedDate { get; init; }
+        public DateTimeOffset LastVisitDate { get; set; }
+    }
+}
---
+++ InventoryAPI/Repositories/InMemClientRepo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using InventoryAPI.Models;
+
+namespace InventoryAPI.Repositories
+{
+    public class InMemClientRepo
+    {
+        private readonly List<Client> items = new()
+        {
+            new Client
+            {
+                Id = Guid.NewGuid(), Name = "Doug LaRose", HorseCount = 2, Location = "Midland",
+                CreatedDate = DateTimeOffset.UtcNow
+            },
+            new Client
+            {
+                Id = Guid.NewGuid(), Name = "Kelly Blouw", HorseCount = 1, Location = "Hudsonville",
+                CreatedDate = DateTimeOffset.UtcNow
+            },
+            new Client
+            {
+                Id = Guid.NewGuid(), Name = "Joe LaCross", HorseCount = 3, Location = "Alma",
+                CreatedDate = DateTimeOffset.UtcNow
+            }
+        };
+
+        public IEnumerable<Client> GetClients()
+        {
+            
+        }
+        
+    }
+}
